a6cb8725b71a2e0af22c88bf11171508d7f4d6ce
Crystal-Spiral <Crystal-Spiral@users.noreply.github.com>
2019-08-01 20:56:02 +0100

    EndGame Display - Currently Borked

---
+++ Ye Troll-ey Problem/Assets/Scripts/EndScores.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScores : MonoBehaviour
+{
+
+    public PlayerCursor player1CursorScript;
+    public PlayerCursor player2CursorScript;
+    public PlayerCursor player3CursorScript;
+    public PlayerCursor player4CursorScript;
+
+    public int[ , ] playerScoreIndexArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScoreIndexArray[0, 0] = player1CursorScript.m_playerID;
+        playerScoreIndexArray[0, 1] = player1CursorScript.m_score;
+
+        playerScoreIndexArray[1, 0] = player2CursorScript.m_playerID;
+        playerScoreIndexArray[1, 1] = player2CursorScript.m_score;
+
+        playerScoreIndexArray[2, 0] = player3CursorScript.m_playerID;
+        playerScoreIndexArray[2, 1] = player3CursorScript.m_score;
+
+        playerScoreIndexArray[3, 0] = player4CursorScript.m_playerID;
+        playerScoreIndexArray[3, 1] = player4CursorScript.m_score;
+
+    }
+
+    
+}
