5664e4d432ea89cb16fbadd8d5bcc98b4ab6c6c6
Ranmara <53568091+Ranmara@users.noreply.github.com>
2019-08-02 12:50:15 +0100

    End scores
    End scores

--- Ye Troll-ey Problem/Assets/Scripts/EndScores.cs
+++ Ye Troll-ey Problem/Assets/Scripts/EndScores.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class EndScores : MonoBehaviour
 {
     [SerializeField]
-    public int[ , ] scoreArray;
 
     public Text firstText;
     public Text secondText;
@@ -15,61 +14,15 @@ public class EndScores : MonoBehaviour
     
     public void DisplayScores(int[] passedScores)
     {
-        if (scoreArray == null)
-            scoreArray = new int[4, 4];
-
-        // [x,0] - player Id, [x,1] - their score
-        scoreArray[0, 0] = 1;
-        scoreArray[0, 1] = passedScores[0];
-
-        scoreArray[1, 0] = 2;
-        scoreArray[1, 1] = passedScores[1];
-
-        scoreArray[2, 0] = 3;
-        scoreArray[2, 1] = passedScores[2];
-
-        scoreArray[3, 0] = 4;
-        scoreArray[3, 1] = passedScores[3];
-
-        ArrangeScores();
         ChangeText();
-
     }
-
-
-    public void ArrangeScores()
-    {
-        //int temp_var = 0;
-
-        //for (int i = 0; i < (scoreArray.Length - 1) ; i++)
-        //{
-        //    for (int j = 0; j < scoreArray.Length; j++)
-        //    {
-        //        if (scoreArray[i,1] < scoreArray[j,1])
-        //        {
-        //            temp_var = scoreArray[i, 0];
-        //            scoreArray[i, 0] = scoreArray[j, 0];
-        //            scoreArray[j, 0] = temp_var;
-
-        //            temp_var = scoreArray[i, 1];
-        //            scoreArray[i, 1] = scoreArray[j, 1];
-        //            scoreArray[j, 1] = temp_var;
-
-        //        }
-        //    }
-
-        //}
-
-    }
-
 
     public void ChangeText()
     {
-        //firstText.text = "Player " + scoreArray[0, 0] + " : " + scoreArray[0, 1];
-        //secondText.text = "Player " + scoreArray[1, 0] + " : " + scoreArray[1, 1];
-        //thirdText.text = "Player " + scoreArray[2, 0] + " : " + scoreArray[2, 1];
-        //fourthText.text = "Player " + scoreArray[3, 0] + " : " + scoreArray[3, 1];
-
+        firstText.text = Game.s_instance.m_scores[0].ToString();
+        secondText.text = Game.s_instance.m_scores[1].ToString();
+        thirdText.text = Game.s_instance.m_scores[2].ToString();
+        fourthText.text = Game.s_instance.m_scores[3].ToString();
     }
     
 }
